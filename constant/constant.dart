a7482a930f41352df8cf2c648d97b54d045d1833
Best <kanin0102@gmail.com>
2024-09-09 15:21:17 +0700

    first commit

---
+++ constant/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const textPrimaryColor = Colors.black;
+const textSecondaryColor = Colors.white;
+final bgPrimaryColor = Colors.blue[200];
+final bgSecondaryColor = Colors.white;
